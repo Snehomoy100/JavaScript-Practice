1dc9786db6caff7052015f0f2741706399e36c8f
snehomoy100 <snehomoy100@gmail.com>
2021-04-24 23:50:15 +0530

    Some files are added

---
+++ TruthyFalsy.js
@@ -0,0 +1,23 @@
+// Falsy values
+
+// undefined;
+// null;
+// 0;
+// NaN;
+// '';
+
+const value = "null";
+
+if (2 == "2") {
+    
+    // type coersion
+    
+    console.log("Yes its true");
+} else {
+    console.log("its false");
+}
+
+
+// console.log(2 + 2);
+// console.log("2" + 2);
+// console.log(2 + '2');
