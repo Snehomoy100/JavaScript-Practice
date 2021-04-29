bed2e4466307dca0527c12dcebf9f7d1bde7d976
snehomoy100 <snehomoy100@gmail.com>
2021-04-29 20:34:44 +0530

    updated

---
+++ Functions.js
@@ -0,0 +1,24 @@
+var x = 1;
+
+a();
+
+b();
+
+console.log(x);
+
+
+// how functions work in JS
+
+
+function a() {
+    var x = 1;
+    console.log(x);
+}
+
+function b() {
+    var x = 1;
+    console.log(x);
+}
+
+
+
