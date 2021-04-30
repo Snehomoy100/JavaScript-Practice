96306bd4c8d23f68d9e327e11471cc74e74d0dd3
snehomoy100 <snehomoy100@gmail.com>
2021-05-01 00:37:15 +0530

    Fully updated

--- Newfile.js
+++ Newfile.js
@@ -4,6 +4,14 @@
 //     var a = 100;
 // };
 
+var a = undefined;
+
+if(a === undefined) {
+    console.log("a is undefined");
+} else {
+    console.log("a is not undefined");
+}
+
 // console.log(x); x is not defined
 
 
