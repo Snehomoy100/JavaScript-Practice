cfe6c934453c6a2a76aea02f09bd8536cc8528cb
snehomoy100 <snehomoy100@gmail.com>
2021-05-01 00:26:14 +0530

    Fully updated

--- Newfile.js
+++ Newfile.js
@@ -1,6 +1,20 @@
-var a = 10;
+// var a = 10;
 
-function b() {
-    var a = 100;
-};
+// function b() {
+//     var a = 100;
+// };
+
+// console.log(x); x is not defined
+
+
+
+// JS is a loosely/weakly typed language.
+var a;
+console.log(a);
+a = 10;
+console.log(a);
+a = "Hi";
+console.log(a);
+a = true;
+console.log(a);
 
